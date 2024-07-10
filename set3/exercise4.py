3b9141f19c6d747b0ed0e143df4e6ab409ae6abf
Yien Yan <5499506@ad.unsw.edu.au>
2024-07-10 14:10:16 +1000

    update

--- set3/exercise4.py
+++ set3/exercise4.py
@@ -22,11 +22,21 @@ def binary_search(low, high, actual_number):
     things much easier.
     """
     tries = 0
-    guess = 0
+
+    while low <= high:
+        guess = (low + high) // 2
+        print(f"Guess:{guess}")
+        tries += 1
+
+        if guess == actual_number:
+            return {"guess": guess, "tries": tries}
+        elif guess < actual_number:
+            low = guess + 1
+        else:
+            high = guess - 1
+    return {"guess": guess, "tries": tries}
 
     # Write your code in here
-
-    return {"guess": guess, "tries": tries}
 
 
 if __name__ == "__main__":
